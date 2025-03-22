d3a5f6e151a1b42a4a6a34c743e85db1a23b5e51
Joshua Zander <joshua@joshz.pro>
2025-03-22 12:53:58 -0600

    Added rust-overlay

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
 
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
 
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     flake-compat = {
       url = "github:nix-community/flake-compat";
       flake = false;
@@ -15,6 +20,7 @@
       self,
       nixpkgs,
       nixpkgs-stable,
+      rust-overlay,
       ...
     }:
     let
@@ -24,6 +30,19 @@
         "x86_64-darwin"
         "aarch64-darwin"
       ];
+      rustPlatformFor =
+        pkgs:
+        if nixpkgs.lib.versionAtLeast pkgs.rustc.version "1.85.1" then
+          pkgs.rustPlatform
+        else
+          let
+            rust-bin = rust-overlay.lib.mkRustBin { } pkgs;
+          in
+          pkgs.makeRustPlatform {
+            cargo = rust-bin.stable.latest.default;
+            rustc = rust-bin.stable.latest.default;
+            cargo-auditable = nixpkgs.legacyPackages.x86_64-linux.cargo-auditable;
+          };
     in
     {
       lib = {
@@ -31,6 +50,7 @@
           pkgs:
           import ./pkgs {
             inherit pkgs;
+            rustPlatform = rustPlatformFor pkgs;
           };
       };
 
@@ -41,6 +61,7 @@
           final: prev:
           import ./pkgs {
             inherit final prev;
+            rustPlatform = rustPlatformFor prev;
           };
       };
 
