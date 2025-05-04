2a764f9016a69c4c6e105857d526f42c7bfa3dd8
Joshua Zander <joshua@joshz.pro>
2025-05-04 02:12:41 -0600

    cosmic-notifications: 1.0.0-alpha.6-unstable-2025-03-27 -> 1.0.0-alpha.7-unstable-2025-05-02
    Diff: https://github.com/pop-os/cosmic-notifications/compare/326ad6800ab5e237761844b0897084d6628531e3...ba66c2b7e2a8245ef2d5ae37e6b9d8d81fe5b631

--- pkgs/cosmic-notifications/package.nix
+++ pkgs/cosmic-notifications/package.nix
@@ -11,17 +11,17 @@
 
 rustPlatform.buildRustPackage {
   pname = "cosmic-notifications";
-  version = "1.0.0-alpha.6-unstable-2025-03-27";
+  version = "1.0.0-alpha.7-unstable-2025-05-02";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-notifications";
-    rev = "326ad6800ab5e237761844b0897084d6628531e3";
-    hash = "sha256-/5JbZMjJwh63gQxmurGaLu5ovGDM58O25KgAYRjJgHg=";
+    rev = "ba66c2b7e2a8245ef2d5ae37e6b9d8d81fe5b631";
+    hash = "sha256-ZJiyCJv0J5zt8A+Q6ymp/daCcbXSmHyGA1OTlSvLbjU=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-7EDe1OjGS6Xl0N/BrZG8lzWO0pFbwiEp+kn6WyydJQM=";
+  cargoHash = "sha256-/DL2htLHGEMA33cboOO3MDFLcdr9sbspfANyjFM6QaM=";
 
   nativeBuildInputs = [
     libcosmicAppHook
