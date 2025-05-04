a42fc8b665f36221398be58ebaddc358763237d4
Joshua Zander <joshua@joshz.pro>
2025-05-04 02:18:14 -0600

    cosmic-term: 1.0.0-alpha.6-unstable-2025-04-03 -> 1.0.0-alpha.7-unstable-2025-05-01
    Diff: https://github.com/pop-os/cosmic-term/compare/00038027c3eae3a46454e6c57a63baf1dca7eb63...c769ae4d7a2b9479943176c9c1178da439185891

--- pkgs/cosmic-term/package.nix
+++ pkgs/cosmic-term/package.nix
@@ -14,17 +14,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cosmic-term";
-  version = "1.0.0-alpha.6-unstable-2025-04-03";
+  version = "1.0.0-alpha.7-unstable-2025-05-01";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-term";
-    rev = "00038027c3eae3a46454e6c57a63baf1dca7eb63";
-    hash = "sha256-4dQrtrIYhh+ZPae4h/G03kSB+mD4GCm5ZrnexxjxCaY=";
+    rev = "c769ae4d7a2b9479943176c9c1178da439185891";
+    hash = "sha256-KKzYism28PL2OL0+g4MwEg0hCbs4WuQVb+EkBmOqONw=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-Iz6RroBEg4OQq4XQ73U4iQK1g5Di2d0cxq8jm977Kxo=";
+  cargoHash = "sha256-Re9t25tkwmrvXB9GmPSVG+QDUZmk5rwrrY2ntlB3Tdw=";
 
   nativeBuildInputs = [
     libcosmicAppHook
