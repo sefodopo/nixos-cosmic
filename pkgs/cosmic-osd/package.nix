537972c55b2c1fd62170787890b3f96aedf169c8
Joshua Zander <joshua@joshz.pro>
2025-05-04 02:13:23 -0600

    cosmic-osd: 1.0.0-alpha.6-unstable-2025-03-28 -> 1.0.0-alpha.7-unstable-2025-05-02
    Diff: https://github.com/pop-os/cosmic-osd/compare/7686175b016054725da1f4fbd0b9f8a149f56e76...8f3477873ab1efbbbbcf221640999d4d757f6cda

--- pkgs/cosmic-osd/package.nix
+++ pkgs/cosmic-osd/package.nix
@@ -11,17 +11,17 @@
 
 rustPlatform.buildRustPackage {
   pname = "cosmic-osd";
-  version = "1.0.0-alpha.6-unstable-2025-03-28";
+  version = "1.0.0-alpha.7-unstable-2025-05-02";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-osd";
-    rev = "7686175b016054725da1f4fbd0b9f8a149f56e76";
-    hash = "sha256-qw4GN5AXOILClqfrjHQzDo4iqq6fic6EViV/SyJH+FQ=";
+    rev = "8f3477873ab1efbbbbcf221640999d4d757f6cda";
+    hash = "sha256-UNrlLnq01gon3/Mtyqay9oV3wKtrYxIjK2uzJmnk3cE=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-kfExKggQo3MoTXw1JbKWjLu5kwYF0n7DzSQcG6e1+QQ=";
+  cargoHash = "sha256-KxRHVjbqTBX6yJmIaHNh7PbEBEiKuNqeliqvucDSx1Y=";
 
   nativeBuildInputs = [
     libcosmicAppHook
