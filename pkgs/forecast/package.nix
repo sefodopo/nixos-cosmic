4ef393d77c5ce6a98871581bc750307d64105c1d
Joshua Zander <joshua@joshz.pro>
2025-05-10 02:05:29 -0600

    cosmic-ext-forecast: 0-unstable-2025-04-12 -> 0-unstable-2025-05-09
    Diff: https://github.com/cosmic-utils/forecast/compare/2dc599ff9a4417d511305d910367dd195aa1378a...47cf79d39c115b025b4a85325abefc8296be6063

--- pkgs/forecast/package.nix
+++ pkgs/forecast/package.nix
@@ -12,17 +12,17 @@
 
 rustPlatform.buildRustPackage {
   pname = "forecast";
-  version = "0-unstable-2025-04-12";
+  version = "0-unstable-2025-05-09";
 
   src = fetchFromGitHub {
     owner = "cosmic-utils";
     repo = "forecast";
-    rev = "2dc599ff9a4417d511305d910367dd195aa1378a";
-    hash = "sha256-sD6aGpU1gET+0YPCD8wtxJWEJgWE/xYTWQAaIu2+e/8=";
+    rev = "47cf79d39c115b025b4a85325abefc8296be6063";
+    hash = "sha256-w8rOndoFO6pCjnAk64YqP5ZViQd5X7LbKsgo+jf3nEo=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-mqZ2tIZzQWU39SMj8UBnScsGAg4xGhkcm51aXx3UBSk=";
+  cargoHash = "sha256-cLObhwMVnaj1HvMhCgSQOYN7IRPKcSeYuAfIy2V5Fns=";
 
   nativeBuildInputs = [
     libcosmicAppHook
