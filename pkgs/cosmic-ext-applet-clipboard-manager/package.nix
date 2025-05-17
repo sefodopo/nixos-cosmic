66caf0d5dc1fcef95434c6c83221d085db124333
Joshua Zander <joshua@joshz.pro>
2025-05-17 02:02:36 -0600

    cosmic-ext-applet-clipboard-manager: 0.1.0-unstable-2025-03-05 -> 0.1.0-unstable-2025-05-17
    Diff: https://github.com/cosmic-utils/clipboard-manager/compare/fcab4b7b8d4934bc8bcdeec33a63628655fd7d57...7adadb2c4608b90eb965b64a4fb67fa7a4a0d576

--- pkgs/cosmic-ext-applet-clipboard-manager/package.nix
+++ pkgs/cosmic-ext-applet-clipboard-manager/package.nix
@@ -10,13 +10,13 @@
 
 rustPlatform.buildRustPackage {
   pname = "cosmic-ext-applet-clipboard-manager";
-  version = "0.1.0-unstable-2025-03-05";
+  version = "0.1.0-unstable-2025-05-17";
 
   src = fetchFromGitHub {
     owner = "cosmic-utils";
     repo = "clipboard-manager";
-    rev = "fcab4b7b8d4934bc8bcdeec33a63628655fd7d57";
-    hash = "sha256-TtYcr2BZCDI9FPHSO22pvWEV/BPiT74JTXhkdBBZrig=";
+    rev = "7adadb2c4608b90eb965b64a4fb67fa7a4a0d576";
+    hash = "sha256-+AJAbw1tOKsteD4RX0mnVIcVoHvRryQ4IUyumQAvyPw=";
   };
 
   useFetchCargoVendor = true;
