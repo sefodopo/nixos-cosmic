6d9df8839eb968f5b55e710d54299e6306946500
Joshua Zander <joshua@joshz.pro>
2025-05-25 19:50:34 -0600

    Updated to nixos-25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
--- nixos/default.nix
+++ nixos/default.nix
@@ -1,6 +1,9 @@
 { cosmicOverlay }:
-
 {
+  disabledModules = [
+    "services/desktop-managers/cosmic.nix"
+    "services/display-managers/cosmic-greeter.nix"
+  ];
   imports = import ./module-list.nix;
 
   nixpkgs.overlays = [ cosmicOverlay ];
