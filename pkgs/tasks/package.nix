b2d7489e3a934a778bf3d7f8f6127379e07202ee
Joshua Zander <joshua@joshz.pro>
2025-06-02 17:14:27 -0600

    cosmic-ext-tasks: 0.1.1-unstable-2025-05-17 -> 0.2.0-unstable-2025-05-31
    Diff: https://github.com/cosmic-utils/tasks/compare/50ce8ae74bca0da4798d8c3658f506f12f2f4c09...638bd4b43f56c945775db7db1b11515a413f8a68

--- pkgs/tasks/package.nix
+++ pkgs/tasks/package.nix
@@ -13,17 +13,17 @@
 
 rustPlatform.buildRustPackage {
   pname = "tasks";
-  version = "0.1.1-unstable-2025-05-17";
+  version = "0.2.0-unstable-2025-05-31";
 
   src = fetchFromGitHub {
     owner = "cosmic-utils";
     repo = "tasks";
-    rev = "50ce8ae74bca0da4798d8c3658f506f12f2f4c09";
-    hash = "sha256-kHVThNYosTEUdXkafoxHm/Igkk/++QZwNxQlpeZ/F34=";
+    rev = "638bd4b43f56c945775db7db1b11515a413f8a68";
+    hash = "sha256-Io7O7utJjtNX6XcUjEXmydezUfDy0SfF5nzNhTjEwTw=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-zW10eDBTGdir5Tj3pvfpjy6CYyK0G1fXSi6ucrU0D6k=";
+  cargoHash = "sha256-zfEx4X5OMuiPP52Ic/VVF8dkRuuC3wx2nXoRoM7c0oo=";
 
   nativeBuildInputs = [
     libcosmicAppHook
