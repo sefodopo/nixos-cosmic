d0a7840ab6d3f5e860504315615cfcbf5d8ae7bc
Joshua Zander <joshua@joshz.pro>
2025-06-02 17:19:29 -0600

    xdg-desktop-portal-cosmic: 1.0.0-alpha.7-unstable-2025-05-07 -> 1.0.0-alpha.7-unstable-2025-05-30
    Diff: https://github.com/pop-os/xdg-desktop-portal-cosmic/compare/34fe50fbeae4eb30be846d9b330411161e957b10...7c9c5dd62f8a5c20d743f812ef2a22aa871fb43b

--- pkgs/xdg-desktop-portal-cosmic/package.nix
+++ pkgs/xdg-desktop-portal-cosmic/package.nix
@@ -16,17 +16,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "xdg-desktop-portal-cosmic";
-  version = "1.0.0-alpha.7-unstable-2025-05-07";
+  version = "1.0.0-alpha.7-unstable-2025-05-30";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "xdg-desktop-portal-cosmic";
-    rev = "34fe50fbeae4eb30be846d9b330411161e957b10";
-    hash = "sha256-hFevZq9fLeRea9/DyQkq5elb3yKJK4BSLEk2/FqgUY0=";
+    rev = "7c9c5dd62f8a5c20d743f812ef2a22aa871fb43b";
+    hash = "sha256-VsBQXU4xhqPiuHwg4YlHLCFeyy9HL89kUgkCe5ooeOQ=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-PJ1AMaxaO1fUXsDMeiUqcEWRCyLEUqwt7YNHk5ymXQ8=";
+  cargoHash = "sha256-7B122CkK32dYSLvXaGonXSMbtP5/Kl3w/8oWdmx3lBY=";
 
   separateDebugInfo = true;
 
