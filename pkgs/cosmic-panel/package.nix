8dbffed3cb94d4a61f12e8e6d8e99cd0707bab0e
Joshua Zander <joshua@joshz.pro>
2025-06-02 17:16:45 -0600

    cosmic-panel: 1.0.0-alpha.7-unstable-2025-05-19 -> 1.0.0-alpha.7-unstable-2025-05-30
    Diff: https://github.com/pop-os/cosmic-panel/compare/36a8e558ea326383821bf50f2bbc5f45c9f41aff...5c48d7f297f737b531fa64c286aa2a120b32cd4c

--- pkgs/cosmic-panel/package.nix
+++ pkgs/cosmic-panel/package.nix
@@ -11,13 +11,13 @@
 
 rustPlatform.buildRustPackage {
   pname = "cosmic-panel";
-  version = "1.0.0-alpha.7-unstable-2025-05-19";
+  version = "1.0.0-alpha.7-unstable-2025-05-30";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-panel";
-    rev = "36a8e558ea326383821bf50f2bbc5f45c9f41aff";
-    hash = "sha256-qIUozExGmFWsvse2BAfz1yCZ7s695OgB6xsQtBJOyyE=";
+    rev = "5c48d7f297f737b531fa64c286aa2a120b32cd4c";
+    hash = "sha256-YwTXTSVNuLeSG/Vz2Ari+3L/BP3TfCvXm8fDld3iaKQ=";
   };
 
   useFetchCargoVendor = true;
