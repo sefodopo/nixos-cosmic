1cc903945563efc0404d23048b413bef94537136
Joshua Zander <joshua@joshz.pro>
2025-09-15 10:48:41 -0600

    cosmic-player: 1.0.0-alpha.7-unstable-2025-05-19 -> 1.0.0-alpha.7-unstable-2025-09-14
    Diff: https://github.com/pop-os/cosmic-player/compare/fadecd0068a3818b6b2072ddc0096d0b8b2293ca...849dd8fabf8f3f05664157cdfa9469ceff7d65aa

--- pkgs/cosmic-player/package.nix
+++ pkgs/cosmic-player/package.nix
@@ -15,17 +15,17 @@
 
 rustPlatform.buildRustPackage {
   pname = "cosmic-player";
-  version = "1.0.0-alpha.7-unstable-2025-05-19";
+  version = "1.0.0-alpha.7-unstable-2025-09-14";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-player";
-    rev = "fadecd0068a3818b6b2072ddc0096d0b8b2293ca";
-    hash = "sha256-WoSkygYF584IS1IX3oipx7agVOu3rT2CKeZuvfoYDJo=";
+    rev = "849dd8fabf8f3f05664157cdfa9469ceff7d65aa";
+    hash = "sha256-18RBAhprkdlafK+mmcjDtGrzG6bCHTW89qia2yA9A7w=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-0RrtErTR2J5mn/Lfppk+5X8UUOC2LN65krAq1uz/O4M=";
+  cargoHash = "sha256-DodFIfthiGFSvXWfPsPjFhNY6G7z3lb6pfc5HtUXhMo=";
 
   nativeBuildInputs = [
     libcosmicAppHook
