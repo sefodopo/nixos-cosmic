677c0cb3b6af70fd1d7a0bf262f21c39b06b7a53
Joshua Zander <joshua@joshz.pro>
2025-09-15 10:45:50 -0600

    cosmic-greeter: 1.0.0-alpha.7-unstable-2025-05-15 -> 1.0.0-alpha.7-unstable-2025-09-14
    Diff: https://github.com/pop-os/cosmic-greeter/compare/b38bcd296a3dfc3cc154e8154ad2824313a67280...f1f7862fbc270ea22db136f8feea027400d3be0f

--- pkgs/cosmic-greeter/package.nix
+++ pkgs/cosmic-greeter/package.nix
@@ -15,17 +15,17 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "cosmic-greeter";
-  version = "1.0.0-alpha.7-unstable-2025-05-15";
+  version = "1.0.0-alpha.7-unstable-2025-09-14";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-greeter";
-    rev = "b38bcd296a3dfc3cc154e8154ad2824313a67280";
-    hash = "sha256-8Lu6TJ7RxuRJsfKqjjyk61GaRV9N/JgPq99sACV9u9Q=";
+    rev = "f1f7862fbc270ea22db136f8feea027400d3be0f";
+    hash = "sha256-Ntq50O++1Lb3w326HjML53sAa4zlxG6tEXHWkjT/ENM=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-alzCbzrFKhG9oIP7Isc+LlVADPCid1NoKPV+wH3DzZg=";
+  cargoHash = "sha256-42In98f6wLkv5rEow6ASditwfnowVTTkvR8kL7c8Dss=";
 
   nativeBuildInputs = [
     libcosmicAppHook
