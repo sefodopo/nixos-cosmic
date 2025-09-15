92f4d6d84a0c853a17dc05c8b295097e402fd0a9
Joshua Zander <joshua@joshz.pro>
2025-09-15 10:43:55 -0600

    cosmic-ext-tweaks: 0.1.3-unstable-2025-06-01 -> 0.1.3-unstable-2025-09-13
    Diff: https://github.com/cosmic-utils/tweaks/compare/a9e478286deb6a11f1df20f367d2f738f8da3592...ec683d517fe240aea47175b7e071244f181d3407

--- pkgs/cosmic-ext-tweaks/package.nix
+++ pkgs/cosmic-ext-tweaks/package.nix
@@ -12,17 +12,17 @@
 
 rustPlatform.buildRustPackage {
   pname = "cosmic-ext-tweaks";
-  version = "0.1.3-unstable-2025-06-01";
+  version = "0.1.3-unstable-2025-09-13";
 
   src = fetchFromGitHub {
     owner = "cosmic-utils";
     repo = "tweaks";
-    rev = "a9e478286deb6a11f1df20f367d2f738f8da3592";
-    hash = "sha256-cUqxTEe6Fseh1Tu/t00ZZcoquQzJ6Ghm6u4c0LZL4Do=";
+    rev = "ec683d517fe240aea47175b7e071244f181d3407";
+    hash = "sha256-q0kK2KbSpd1aZapfrEkaRfwqgZ0NmOwghYzG3kAbKVc=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-5heV0xZyVbWvMzZnKzT9o//bbzX8g6NuhPgnzw68/pU=";
+  cargoHash = "sha256-X1ER0iYp0s2ap3FQfvXrXTyFOo1QdW+ftNaNvtYdjRs=";
 
   nativeBuildInputs = [
     libcosmicAppHook
